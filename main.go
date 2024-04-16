5483efaa7c49a8abaca6df6837fd26d7079dbc3c
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-04-16 17:26:09 +0200

    Simple fileserver

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	const FilePathRoot = "."
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(FilePathRoot)))
+	corsMux := middlewareCors(mux)
+	srv := &http.Server{
+		Handler: corsMux,
+		Addr:    ":" + port,
+	}
+	log.Printf("Serving files from %s on port %s", FilePathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
